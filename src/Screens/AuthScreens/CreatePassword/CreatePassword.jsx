551e849d5bc6f0c10062ad4f5120dc970f8ddfbe
BugDev Frontend <120487197+BugdevFrontend1@users.noreply.github.com>
2024-07-20 17:18:28 +0500

    SwissPort Project Setup

---
+++ src/Screens/AuthScreens/CreatePassword/CreatePassword.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const CreatePassword = () => {
+  return (
+    <View>
+      <Text>CreatePassword</Text>
+    </View>
+  );
+};
+
+export default CreatePassword;
---
+++ src/Screens/AuthScreens/VerifyEmail/VerifyEmail.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const VerifyEmail = () => {
+  return (
+    <View>
+      <Text>VerifyEmail</Text>
+    </View>
+  );
+};
+
+export default VerifyEmail;
